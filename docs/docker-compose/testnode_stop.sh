a71029a3f9cc2227e9ee6a7098bf9fa10f6afd78
Zack Cerza <zack@redhat.com>
2022-02-03 17:04:59 -0700

    testnode: Add stop script

---
+++ docs/docker-compose/testnode_stop.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+set -x
+payload="{\"name\": \"$(hostname)\", \"machine_type\": \"testnode\", \"up\": false}"
+for i in $(seq 1 5); do
+    echo "attempt $i"
+    curl -v -X POST -d "$payload" http://paddles:8080/nodes/ && break
+    sleep 1
+done
+pkill sshd
